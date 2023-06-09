fe9d20da6e8d51e11aa1c26321dae51ba53b32cb
Farm-Boy7130 <104338902+Farm-Boy7130@users.noreply.github.com>
2023-06-09 16:38:03 -0500

    Created ReadMe.cpp

---
+++ ReadMe.cpp
@@ -0,0 +1,8 @@
+/*
+Hi! If you're reading this then chances are you're probably a nerd, interested in calculating the dispalcement of engines quickly and easily. Or maybe just somebody who's really bored.
+Either which way, my program is easy to use, user friendly, functional, and most important, Open-Sourced. It's written 100% in C++.
+There are other things you can use this program for, such as calculating compression ratios of engines (WIP).
+I hope my program brings you a lot of help and lessens your frustration whenever you can't remember how to do math. Thanks for using my program!
+
+Copyright (c) 2023 Farm-Boy7130
+*/
